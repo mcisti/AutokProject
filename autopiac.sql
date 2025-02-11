76c078d05bc247d07d2c8f375bc085a0884d11e2
Halaszl <90753648+Halaszl@users.noreply.github.com>
2025-02-11 10:05:28 +0100

    Add files via upload

---
+++ autopiac.sql
@@ -0,0 +1,248 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2025. Feb 11. 10:04
+-- Kiszolgáló verziója: 10.4.20-MariaDB
+-- PHP verzió: 7.3.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `autopiac`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `jarmuvek`
+--
+
+CREATE TABLE `jarmuvek` (
+  `id` int(11) NOT NULL,
+  `marka` varchar(100) DEFAULT NULL,
+  `tipus` varchar(100) DEFAULT NULL,
+  `evjarat` int(11) DEFAULT NULL,
+  `kilometer` int(11) DEFAULT NULL,
+  `szin` varchar(50) DEFAULT NULL,
+  `motor_tipus` varchar(50) DEFAULT NULL,
+  `motor_meret` decimal(4,2) DEFAULT NULL,
+  `teljesitmeny` int(11) DEFAULT NULL,
+  `sebessegvalto` varchar(50) DEFAULT NULL,
+  `ar` decimal(10,2) DEFAULT NULL,
+  `allapot` text DEFAULT NULL,
+  `felszereltseg` text DEFAULT NULL,
+  `muszaki_vizsga` date DEFAULT NULL,
+  `elojel` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- A tábla adatainak kiíratása `jarmuvek`
+--
+
+INSERT INTO `jarmuvek` (`id`, `marka`, `tipus`, `evjarat`, `kilometer`, `szin`, `motor_tipus`, `motor_meret`, `teljesitmeny`, `sebessegvalto`, `ar`, `allapot`, `felszereltseg`, `muszaki_vizsga`, `elojel`) VALUES
+(1, 'Toyota', 'Corolla', 2015, 120000, 'Fehér', 'Benzin', '1.60', 132, 'Automatikus', '3500000.00', 'Jó állapot', 'Klíma, Navigáció', '2025-12-01', 'Eladó'),
+(2, 'BMW', 'M5 Competition', 2022, 165000, 'Fekete', 'Benzin', '3.00', 530, 'Automatikus', '29000000.00', 'Megkímélt', 'Digitális Klíma, Lemeztető, Összkerék', '2025-02-01', 'Eladó Garanciális');
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `kapcsolatok`
+--
+
+CREATE TABLE `kapcsolatok` (
+  `id` int(11) NOT NULL,
+  `jarmu_id` int(11) DEFAULT NULL,
+  `kapcsolattarto_nev` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `telefon` varchar(20) DEFAULT NULL,
+  `uzenet` text DEFAULT NULL,
+  `datum` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `kepek_video`
+--
+
+CREATE TABLE `kepek_video` (
+  `id` int(11) NOT NULL,
+  `jarmu_id` int(11) DEFAULT NULL,
+  `tipus` enum('kep','video') DEFAULT NULL,
+  `eleresi_ut` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `keresesi_informaciok`
+--
+
+CREATE TABLE `keresesi_informaciok` (
+  `id` int(11) NOT NULL,
+  `jarmu_id` int(11) DEFAULT NULL,
+  `keresett_feltetel` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `torteneti_adatok`
+--
+
+CREATE TABLE `torteneti_adatok` (
+  `id` int(11) NOT NULL,
+  `jarmu_id` int(11) DEFAULT NULL,
+  `elozo_tulajdonosok` int(11) DEFAULT NULL,
+  `baleset` tinyint(1) DEFAULT NULL,
+  `szerviztortenet` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `velemenyek`
+--
+
+CREATE TABLE `velemenyek` (
+  `id` int(11) NOT NULL,
+  `jarmu_id` int(11) DEFAULT NULL,
+  `vasarlo_nev` varchar(100) DEFAULT NULL,
+  `velemeny` text DEFAULT NULL,
+  `ertekeles` int(11) DEFAULT NULL,
+  `datum` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `jarmuvek`
+--
+ALTER TABLE `jarmuvek`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `kapcsolatok`
+--
+ALTER TABLE `kapcsolatok`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `jarmu_id` (`jarmu_id`);
+
+--
+-- A tábla indexei `kepek_video`
+--
+ALTER TABLE `kepek_video`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `jarmu_id` (`jarmu_id`);
+
+--
+-- A tábla indexei `keresesi_informaciok`
+--
+ALTER TABLE `keresesi_informaciok`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `jarmu_id` (`jarmu_id`);
+
+--
+-- A tábla indexei `torteneti_adatok`
+--
+ALTER TABLE `torteneti_adatok`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `jarmu_id` (`jarmu_id`);
+
+--
+-- A tábla indexei `velemenyek`
+--
+ALTER TABLE `velemenyek`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `jarmu_id` (`jarmu_id`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `jarmuvek`
+--
+ALTER TABLE `jarmuvek`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT a táblához `kapcsolatok`
+--
+ALTER TABLE `kapcsolatok`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `kepek_video`
+--
+ALTER TABLE `kepek_video`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `keresesi_informaciok`
+--
+ALTER TABLE `keresesi_informaciok`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `torteneti_adatok`
+--
+ALTER TABLE `torteneti_adatok`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `velemenyek`
+--
+ALTER TABLE `velemenyek`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Megkötések a kiírt táblákhoz
+--
+
+--
+-- Megkötések a táblához `kapcsolatok`
+--
+ALTER TABLE `kapcsolatok`
+  ADD CONSTRAINT `kapcsolatok_ibfk_1` FOREIGN KEY (`jarmu_id`) REFERENCES `jarmuvek` (`id`);
+
+--
+-- Megkötések a táblához `kepek_video`
+--
+ALTER TABLE `kepek_video`
+  ADD CONSTRAINT `kepek_video_ibfk_1` FOREIGN KEY (`jarmu_id`) REFERENCES `jarmuvek` (`id`);
+
+--
+-- Megkötések a táblához `keresesi_informaciok`
+--
+ALTER TABLE `keresesi_informaciok`
+  ADD CONSTRAINT `keresesi_informaciok_ibfk_1` FOREIGN KEY (`jarmu_id`) REFERENCES `jarmuvek` (`id`);
+
+--
+-- Megkötések a táblához `torteneti_adatok`
+--
+ALTER TABLE `torteneti_adatok`
+  ADD CONSTRAINT `torteneti_adatok_ibfk_1` FOREIGN KEY (`jarmu_id`) REFERENCES `jarmuvek` (`id`);
+
+--
+-- Megkötések a táblához `velemenyek`
+--
+ALTER TABLE `velemenyek`
+  ADD CONSTRAINT `velemenyek_ibfk_1` FOREIGN KEY (`jarmu_id`) REFERENCES `jarmuvek` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
