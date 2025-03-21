4aa77048870d5b17f6fbeb3e41f7ed890113708b
feedadam <90382294+mcisti@users.noreply.github.com>
2025-03-21 07:39:41 +0100

    Delete autoprojekt directory

--- autoprojekt/Models/JwtOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace autoprojekt.Models
-{
-    public class JwtOptions
-    {
-        public string Issuer { get; set; }
-        public string Audience { get; set; }
-        public string Secret { get; set; }
-    }
-}
--- autoprojekt/Services/IAuthService/ITokenGenerator.cs
+++
@@ -1,9 +0,0 @@
-﻿using autoprojekt.Models;
-
-namespace autoprojekt.Services.IAuthService
-{
-    public interface ITokenGenerator
-    {
-        string GenerateToken(ApplicationUser applicationUser, IEnumerable<string> role);
-    }
-}
